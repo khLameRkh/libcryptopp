ab71e71f967c47fb4a867b789e403d7e6ff9ba1e
weishijun <wsj@wiz.cn>
2013-04-18 15:47:53 +0800

    init the project

---
+++ cryptopp/cryptopp.h
@@ -0,0 +1,13 @@
+//
+//  cryptopp.h
+//  cryptopp
+//
+//  Created by dzpqzb on 13-4-15.
+//  Copyright (c) 2013年 dzpqzb.cn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cryptopp : NSObject
+
+@end
---
+++ cryptopp/cryptopp.m
@@ -0,0 +1,13 @@
+//
+//  cryptopp.m
+//  cryptopp
+//
+//  Created by dzpqzb on 13-4-15.
+//  Copyright (c) 2013年 dzpqzb.cn. All rights reserved.
+//
+
+#import "cryptopp.h"
+
+@implementation cryptopp
+
+@end
